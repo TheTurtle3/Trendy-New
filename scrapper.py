2f2c8120907d0828a485e737e53865e17e206bb2
Ahmad Zafar <amz9616@gmail.com>
2022-08-09 19:28:43 -0400

    added a db, shows are now included, better method for scraping title

--- scrapper.py
+++ scrapper.py
@@ -1,27 +1,56 @@
 import requests
 from bs4 import BeautifulSoup
+import sqlite3
 
-# url for the most popular movies on imdb
-url = "https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm"
 
+# creating a db 
+con = sqlite3.connect("trendynew.db")
+cur = con.cursor()
+cur.execute('''CREATE TABLE IF NOT EXISTS movies(title text, poster text)''')
+cur.execute('''CREATE TABLE IF NOT EXISTS shows(title text, poster text)''')
+
+
+# clearing the tables before adding new data
+cur.execute("DELETE FROM movies")
+cur.execute("DELETE FROM shows")
+
+
+# setting up scraper for most popular movies
+url = "https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm" # imdb url for most popular movies
 r = requests.get(url)
 c = r.content
-
 soup = BeautifulSoup(c, "html.parser")
 
-# scraping for movie poster
-posters = []
+
+# scraping for movie poster src (instead of going into the seperate div to get title of the movie as before,
+# it is easier and more efficient to get the 'alt' attribute of the img as it is the title of the movie).
 all = soup.find_all("td", {"class": "posterColumn"})
 for item in all:
-    poster = item.find("img")
-    posters.append(poster.get("src"))
+    img = item.find("img")
+    imgsrc = img.get("src")
+    title = img.get("alt")
 
-# scraping for movie title
-titles = []
-all = soup.find_all("td", {"class": "titleColumn"})
+    # adding the name and imgsrc to db
+    cur.execute("INSERT INTO movies VALUES (?, ?)", (title, imgsrc))
+
+
+# setting up scraper for most popular shows
+url = "https://www.imdb.com/chart/tvmeter/?ref_=nv_tvv_mptv" # imdb url to most popular shows
+r = requests.get(url)
+c = r.content
+soup = BeautifulSoup(c, "html.parser")
+
+# scraping for show poster src and show title
+all = soup.find_all("td", {"class": "posterColumn"})
 for item in all:
-    title = item.find("a").text
-    titles.append(title)
+    img = item.find("img")
+    imgsrc = img.get("src")
+    title = img.get("alt")
+    
+    # adding the name and imgsrc to db
+    cur.execute("INSERT INTO shows VALUES (?, ?)", (title, imgsrc))
 
-# testing
-print(posters)
+
+# commiting changes and closing the connection to database
+con.commit()
+con.close()
