96bf024b3f9abd12efb995e60b396ea89a587161
Ahmad Zafar <amz9616@gmail.com>
2022-08-09 16:03:09 -0400

    The names and image are stored in a python list

--- scrapper.py
+++ scrapper.py
@@ -5,12 +5,23 @@ from bs4 import BeautifulSoup
 url = "https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm"
 
 r = requests.get(url)
-
 c = r.content
 
 soup = BeautifulSoup(c, "html.parser")
-all = soup.find_all("tbody", {"class": "lister-list"})
 
+# scraping for movie poster
+posters = []
+all = soup.find_all("td", {"class": "posterColumn"})
+for item in all:
+    poster = item.find("img")
+    posters.append(poster.get("src"))
 
+# scraping for movie title
+titles = []
+all = soup.find_all("td", {"class": "titleColumn"})
+for item in all:
+    title = item.find("a").text
+    titles.append(title)
 
-print(all)
+# testing
+print(posters)
